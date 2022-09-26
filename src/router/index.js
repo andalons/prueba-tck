accdf0885cd6b3b3f291e43a38081ea1ad59ea35
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-09-26 11:28:43 +0200

    clean vue template

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Tab1 from '../views/Tab1.vue'
+import Tab2 from '../views/Tab2.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'tab-1',
+      component: Tab1
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/tab-2',
+      name: 'tab-2',
+    component: Tab2
     }
   ]
 })
