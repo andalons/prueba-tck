303e2074617c88395888af46ec857e8e1ee5fc32
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-09-27 15:46:14 +0200

    changes in components logic and structure

--- src/components/tab1/hotel-plan-card/HotelPlanCard.vue
+++ src/components/tab1/hotel-plan-card/HotelPlanCard.vue
@@ -1,7 +1,3 @@
-<script setup>
-const props = defineProps(["title", "services", "id"]);
-</script>
-
 <template>
   <article>
     <h3>{{ title }}</h3>
@@ -9,14 +5,32 @@ const props = defineProps(["title", "services", "id"]);
       <li v-for="service in services">{{ service }}</li>
     </ul>
     <div>
-      <label :for="id">
-        <input :id="id" type="radio" :value="title" name="plans" />
-        Elegir régimen
-      </label>
+      <!-- esto iría dentro del input @change="toggleClass"  -->
+      <input
+        :id="id"
+        type="radio"
+        :value="title"
+        name="plans"
+        v-model="selectedPlan"
+      />
+      <label :for="id">Elegir régimen</label>
     </div>
   </article>
 </template>
 
-<style lang="scss">
-@import "./HotelPlanCard.scss";
-</style>
+<script setup>
+import { ref, defineEmits } from "vue";
+/* const emit = defineEmits(["active-card"]);*/
+const props = defineProps(["title", "services", "id"]);
+
+const selectedPlan = ref("");
+/* const toggleClass = (e) => {
+  console.log("events:", e, e.target.id);
+  emit("active-card", e.target.id);
+  console.log(selectedPlan);
+  console.log(e.target.value);
+  console.log(props.title);
+}; */
+</script>
+
+<style lang="scss" src="./HotelPlanCard.scss"></style>
--- src/components/tab1/hotel-plan-list/HotelPlanList.vue
+++ src/components/tab1/hotel-plan-list/HotelPlanList.vue
@@ -1,11 +1,8 @@
-<script setup>
-import HotelPlanCard from "../hotel-plan-card/HotelPlanCard.vue";
-const props = defineProps(["hotelPlans"]);
-</script>
-
 <template>
   <ul>
     <li v-for="plan in hotelPlans" :key="plan.id">
+      <!--  :class="[plan.id === planId ? 'isActive' : '']" -->
+      <!-- @active-card="toggleClass2" -->
       <HotelPlanCard
         :title="plan.title"
         :services="plan.services"
@@ -15,6 +12,15 @@ const props = defineProps(["hotelPlans"]);
   </ul>
 </template>
 
-<style lang="scss">
-@import "./HotelPlanList.scss";
-</style>
+<script setup>
+/* import { ref } from "vue"; */
+import HotelPlanCard from "../hotel-plan-card/HotelPlanCard.vue";
+const props = defineProps(["hotelPlans"]);
+/* const planId = ref(1);
+const toggleClass2 = (e) => {
+  console.log("recibo de emits:", e);
+  planId = e;
+}; */
+</script>
+
+<style lang="scss" src="./HotelPlanList.scss"></style>
