0434b8b1ccd5f465bcecfb0ccefe641a590cb4e8
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-09-27 06:51:30 +0200

    change header import in App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from "vue-router";
 
-import AppHeader from "./components/header/AppHeader.vue";
+import AppHeader from "./components/app-header/AppHeader.vue";
 </script>
 
 <template>
