0e749e05f235daa502e5bb994ded65e00756b4a8
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-09-27 15:44:48 +0200

    change data to ref variable

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,15 +1,15 @@
-<script setup>
-import data from "@/data/hotelPlans.json";
-import HotelPlanList from "../components/tab1/hotel-plan-list/HotelPlanList.vue";
-
-const hotelPlans = data;
-</script>
-
 <template>
   <section class="tab1 tab-main">
     <h2>Hotel Mieres del Camín Apartamentos</h2>
     <HotelPlanList :hotelPlans="hotelPlans" />
   </section>
 </template>
+
+<script setup>
+import plansData from "@/data/hotelPlans.json";
+import HotelPlanList from "../components/tab1/hotel-plan-list/HotelPlanList.vue";
+import { ref } from "vue";
+const hotelPlans = ref(plansData);
+</script>
 
 <style></style>
