c56afeb4882b1b2e8a74a71170c3903f46e4bb76
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-09-30 10:33:09 +0200

    set basic data for Tab2

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,9 +1,19 @@
-<script></script>
-
 <template>
-  <section class="tab2 tab-main">
-    <h2>Bebidas</h2>
+  <section class="tab2">
+    <h2 class="tab2__title">Bebidas</h2>
+    <p v-for="beer in beersData" :key="beer.id">{{ beer.name }}</p>
   </section>
 </template>
 
-<style></style>
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+const beersData = ref([]);
+(async () => {
+  const res = await axios.get("https://api.punkapi.com/v2/beers?per_page=10");
+  beersData.value = res.data;
+  console.log(res);
+})();
+</script>
+
+<style lang="scss" src="./Tab2.scss"></style>
