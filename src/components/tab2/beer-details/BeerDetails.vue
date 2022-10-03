225204732940f7c3770172a7ea77238a5f529a9d
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-10-03 14:17:22 +0200

    add styles for Tab2

--- src/components/tab2/beer-details/BeerDetails.vue
+++ src/components/tab2/beer-details/BeerDetails.vue
@@ -1,27 +1,36 @@
 <template>
-  <article>
-    <img src="" alt="" />
-    <div class="">
-      <h3>{{ name }}</h3>
-      <p>{{ description }}</p>
-      <p>
-        Ideal to pair with:
-        <span v-for="(pairing, index) in pairings" key="index"
-          >{{ pairing }}<span v-if="index < pairings.length - 1">, </span>
-          <span v-else>.</span>
-        </span>
-      </p>
+  <article class="beer-details">
+    <div class="beer-details__img-container">
+      <img :src="img" :alt="name" class="image" />
     </div>
-    <div>{{ alcohol }}</div>
+    <div class="beer-details__info-container">
+      <div class="top">
+        <h3 class="title">{{ name }}</h3>
+        <p class="description">{{ description }}</p>
+      </div>
+      <div class="bottom">
+        <p class="pairing">
+          Ideal to pair with:
+          <span v-for="(pairing, index) in pairings" key="index"
+            >{{ pairing }}<span v-if="index < pairings.length - 1">, </span>
+            <span v-else>.</span>
+          </span>
+        </p>
+      </div>
+    </div>
+    <div class="beer-details__abv-container">
+      <p class="abv" :class="defineAbvColor(abv)">{{ abv }}</p>
+    </div>
   </article>
 </template>
 
 <script setup>
-const props = defineProps([
-  "img",
-  "name",
-  "description",
-  "pairings",
-  "alcohol",
-]);
+const props = defineProps(["img", "name", "description", "pairings", "abv"]);
+function defineAbvColor(abv) {
+  if (abv <= "5") return "abv--low";
+  else if (abv >= "5.1" && abv <= "10") return "abv--med";
+  else if (abv > "10") return "abv--high";
+}
 </script>
+
+<style lang="scss" src="./BeerDetails.scss"></style>
--- src/components/tab2/beers-list/BeersList.vue
+++ src/components/tab2/beers-list/BeersList.vue
@@ -1,15 +1,18 @@
 <template>
-  <ul class="beers-list">
-    <li class="beers-list__item" v-for="beer in beers" :key="beer.id">
-      <BeerDetails
-        :img="beer.imgage_url"
-        :name="beer.name"
-        :description="beer.description"
-        :pairings="beer.food_pairing"
-        :alcohol="beer.abv"
-      />
-    </li>
-  </ul>
+  <section>
+    <h2 class="beers-title">Cervezas</h2>
+    <ul class="beers-list">
+      <li class="beers-list__item" v-for="beer in beers" :key="beer.id">
+        <BeerDetails
+          :img="beer.image_url"
+          :name="beer.name"
+          :description="beer.description"
+          :pairings="beer.food_pairing"
+          :abv="beer.abv"
+        />
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
@@ -17,3 +20,5 @@ import BeerDetails from "../beer-details/BeerDetails.vue";
 
 const props = defineProps(["beers"]);
 </script>
+
+<style lang="scss" src="./BeersList.scss"></style>
