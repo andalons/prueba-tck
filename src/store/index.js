33ef33ac8edbc38f6e561ede706395e77c98476a
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-10-08 10:32:53 +0200

    add api call and save data in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
 import { createStore} from 'vuex'
+
 export default createStore({
     state(){
         return {
-            
+            beersData: [],
+            shoppingCart: {},
         }
     },
     mutations:{
-
+        setBeersData(state, payload) {
+            state.beersData = payload
+        }
     },
     actions: {
-
+        async fetchData({commit}) {
+            try {
+              const res = await fetch('https://api.punkapi.com/v2/beers?per_page=10')
+              const beers = await res.json()
+              commit('setBeersData', beers)
+            } catch (error) {
+              console.log(error)
+            }
+          },
     },
     getters: {
 
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,25 +1,28 @@
 <template>
   <section class="tab2">
     <h2 class="tab2__title">Bebidas</h2>
-    <BeersList class="beers-container" :beers="beersData" />
+    <pre>{{ beersData }}</pre>
+    <!--  <BeersList class="beers-container" /> -->
   </section>
 </template>
 
 <script setup>
-import axios from "axios";
-import { ref, onMounted } from "vue";
+//import axios from "axios";
+import { onMounted, computed } from "vue";
+import { useStore } from "vuex";
 import BeersList from "../components/tab2/beers-list/BeersList.vue";
+/*
 const beersData = ref([]);
-/* (async () => {
-  const res = await axios.get("https://api.punkapi.com/v2/beers?per_page=10");
-  beersData.value = res.data;
-  console.log(res);
-})(); */
 onMounted(async () => {
   const res = await axios.get("https://api.punkapi.com/v2/beers?per_page=10");
   beersData.value = res.data;
   console.log(res);
+});*/
+const store = useStore();
+onMounted(async () => {
+  store.dispatch("fetchData");
 });
+const beersData = computed(() => store.state.beersData);
 </script>
 
 <style lang="scss" src="./Tab2.scss"></style>
