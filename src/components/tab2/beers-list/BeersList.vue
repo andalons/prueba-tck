3f4705201e9e26bec7a87a20c2bb0ad81c5ba8b4
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-10-08 10:53:02 +0200

    get data from store in component

--- src/components/tab2/beers-list/BeersList.vue
+++ src/components/tab2/beers-list/BeersList.vue
@@ -1,8 +1,9 @@
 <template>
   <section>
     <h2 class="beers-title">Cervezas</h2>
+    <!--   <pre>{{ beersData }}</pre> -->
     <ul class="beers-list">
-      <li class="beers-list__item" v-for="beer in beers" :key="beer.id">
+      <li class="beers-list__item" v-for="beer in beersData" :key="beer.id">
         <BeerDetails
           :img="beer.image_url"
           :name="beer.name"
@@ -17,8 +18,11 @@
 
 <script setup>
 import BeerDetails from "../beer-details/BeerDetails.vue";
+import { useStore } from "vuex";
+import { computed } from "vue";
 
-const props = defineProps(["beers"]);
+const store = useStore();
+const beersData = computed(() => store.state.beersData);
 </script>
 
 <style lang="scss" src="./BeersList.scss"></style>
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,28 +1,20 @@
 <template>
   <section class="tab2">
     <h2 class="tab2__title">Bebidas</h2>
-    <pre>{{ beersData }}</pre>
-    <!--  <BeersList class="beers-container" /> -->
+
+    <BeersList class="beers-container" />
   </section>
 </template>
 
 <script setup>
-//import axios from "axios";
-import { onMounted, computed } from "vue";
+import { onMounted } from "vue";
 import { useStore } from "vuex";
 import BeersList from "../components/tab2/beers-list/BeersList.vue";
-/*
-const beersData = ref([]);
-onMounted(async () => {
-  const res = await axios.get("https://api.punkapi.com/v2/beers?per_page=10");
-  beersData.value = res.data;
-  console.log(res);
-});*/
+
 const store = useStore();
 onMounted(async () => {
   store.dispatch("fetchData");
 });
-const beersData = computed(() => store.state.beersData);
 </script>
 
 <style lang="scss" src="./Tab2.scss"></style>
