8e2ee11d523eda863e7b3dcd3e9ef76e0ae93d89
‘Andrea <andrea.alonso.corbeira@the-cocktail.com>
2022-10-08 10:09:42 +0200

    add store to project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+import store from './store'
 import App from './App.vue'
 import router from './router'
-
 import '@/assets/styles/_main.scss'
+
+
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(store)
 
 app.mount('#app')
